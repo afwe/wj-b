a0e2b2637d4a03893db15f2b9e98abebf7686414
afwe <936017948@qq.com>
2020-11-10 15:19:52 +0800

    go

---
+++ src/main/java/com/wj/evan/controller/RegistController.java
@@ -0,0 +1,52 @@
+package com.wj.evan.controller;
+
+import com.wj.evan.pojo.User;
+import com.wj.evan.result.Result;
+import com.wj.evan.result.ResultFactory;
+import com.wj.evan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
+import org.apache.shiro.crypto.*;
+import org.apache.shiro.crypto.hash.*;
+
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class RegistController {
+
+    @Autowired
+    private UserService userService;
+
+    @CrossOrigin
+    @PostMapping(value = "/api/register")
+    @ResponseBody
+    public Result register(@RequestBody User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        username = HtmlUtils.htmlEscape(username);
+        user.setUsername(username);
+
+        boolean exist = userService.isExist(username);
+        if (exist) {
+            String message = "用户名已被使用";
+            return ResultFactory.buildFailResult(message);
+        }
+
+        // 生成盐,默认长度 16 位
+        String salt = new SecureRandomNumberGenerator().nextBytes().toString();
+        // 设置 hash 算法迭代次数
+        int times = 2;
+        // 得到 hash 后的密码
+        String encodedPassword = new SimpleHash("md5", password, salt, times).toString();
+        // 存储用户信息，包括 salt 与 hash 后的密码
+        user.setSalt(salt);
+        user.setPassword(encodedPassword);
+        userService.add(user);
+
+        return ResultFactory.buildSuccessResult(user);
+    }
+
+}
---
+++ src/main/java/com/wj/evan/result/ResultFactory.java
@@ -0,0 +1,23 @@
+package com.wj.evan.result;
+
+import com.wj.evan.result.Result;
+
+public class ResultFactory {
+
+    public static Result buildSuccessResult(Object data) {
+        return buildResult(ResultCode.SUCCESS, "成功", data);
+    }
+
+    public static Result buildFailResult(String message) {
+        return buildResult(ResultCode.FAIL, message, null);
+    }
+
+    public static Result buildResult(ResultCode resultCode, String message, Object data) {
+
+        return new Result(resultCode.code, message, data);
+    }
+
+    /***public static Result buildResult(int resultCode, String message, Object data) {
+        return new Result(resultCode, message, data);
+    }*/
+}
